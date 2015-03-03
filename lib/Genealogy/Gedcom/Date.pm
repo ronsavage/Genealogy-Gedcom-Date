01e73186a9e2ee3a7ce8001fb1866dfcea1a068f
ronsavage <ron@savage.net.au>
2015-03-03 15:57:11 +1100

    Update Build.PL and Makefile.PL

--- lib/Genealogy/Gedcom/Date.pm
+++ lib/Genealogy/Gedcom/Date.pm
@@ -1426,7 +1426,7 @@ See L<Genealogy::Gedcom::Reader::Lexer/References>.
 
 =head1 Machine-Readable Change Log
 
-The file CHANGES was converted into Changelog.ini by L<Module::Metadata::Changes>.
+The file Changes was converted into Changelog.ini by L<Module::Metadata::Changes>.
 
 =head1 Version Numbers
 
